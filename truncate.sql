295f6a0b00f28d47db88a0b28c3e7909266f9fb8
Jeffrey Y. Pogoy <jypogoy@gmail.com>
2018-04-10 08:20:20 +0800

    Created separate diagrams in wms.

--- truncate.sql
+++ truncate.sql
@@ -1,5 +1,5 @@
-set foreign_key_checks = 0;
+set foreign_key_checks=0;
+truncate data_entry;
 truncate merchant_header;
 truncate `transaction`;
-truncate data_entry;
-update batch set entry_status = null, verify_status = null, is_exception = null where id > 0;
+update batch set entry_status = null, verify_status = null, is_exception = null where id < 20;
