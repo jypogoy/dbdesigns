f6066f1027bfe426d7ca178a8be018e186cc69fa
Jeffrey Y. Pogoy <jypogoy@gmail.com>
2018-04-12 10:14:04 +0800

    Added balancing column in data_entry table

--- truncate.sql
+++ truncate.sql
@@ -2,4 +2,4 @@ set foreign_key_checks=0;
 truncate data_entry;
 truncate merchant_header;
 truncate `transaction`;
-update batch set entry_status = null, verify_status = null, is_exception = null where id < 20;
+update batch set entry_status = null, verify_status = null, balance_status = null, is_exception = null where id < 20;
