2ecd7e96f6d86ebe093733348583d41132d1860e
Jeffrey Pogoy <jypogoy@gmail.com>
2018-07-04 21:21:58 +0800

    Added new gpap dump dated june 20

--- truncate.sql
+++ truncate.sql
@@ -2,4 +2,5 @@ set foreign_key_checks=0;
 truncate data_entry;
 truncate merchant_header;
 truncate `transaction`;
+truncate dcn;
 update batch set entry_status = null, verify_status = null, balance_status = null, is_exception = null where id < 20;
